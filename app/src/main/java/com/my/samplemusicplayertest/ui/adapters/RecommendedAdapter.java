adf10c4f5ffb5049aa77af934761dafda18dc15e
dpn111 <129816474+noobdevneedhelp@users.noreply.github.com>
2024-04-14 19:17:59 +0700

    Update RecommendedAdapter.java

--- app/src/main/java/com/my/samplemusicplayertest/ui/adapters/RecommendedAdapter.java
+++ app/src/main/java/com/my/samplemusicplayertest/ui/adapters/RecommendedAdapter.java
@@ -1,4 +1,4 @@
-package com.example.musicplayerapp;
+package com.my.samplemusicplayertest.ui.adapters;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.musicplayerapp.model.Recommended;
+import com.my.samplemusicplayertest.R;
 
 import java.util.List;
 
