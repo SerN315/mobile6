6f85d1b9576f711e97bd12f5d39951a4fa65e18d
SerN315 <129875169+SerN315@users.noreply.github.com>
2024-04-14 22:30:26 +0700

    them nut noi vao search

--- app/src/main/java/com/my/samplemusicplayertest/ui/fragments/FragmentHome.java
+++ app/src/main/java/com/my/samplemusicplayertest/ui/fragments/FragmentHome.java
@@ -1,15 +1,18 @@
 package com.my.samplemusicplayertest.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.my.samplemusicplayertest.R;
+import com.my.samplemusicplayertest.SearchActivity;
 
 public class FragmentHome extends Fragment {
 
@@ -22,5 +25,13 @@ public class FragmentHome extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ImageView searchImageView = view.findViewById(R.id.search);
+        searchImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
